8329bdf2346dedd21276401c0bd4429e3aa91db5
JB Adams <JBAdams@JBs-MacBook-Pro.local>
2014-03-09 11:30:22 -0500

    Initial Commit

---
+++ NutrientMgmt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NutrientMgmt
+//
+//  Created by JB Adams on 3/9/14.
+//  Copyright St.Croix Design Lab 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
